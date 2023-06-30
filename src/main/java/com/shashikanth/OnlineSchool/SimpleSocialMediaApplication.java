243ba37dea4cdc07442b1bf4cd6d616c2f2a1e02
shashi <mallepally.shashikanthh@yahoo.com>
2023-06-30 16:56:59 +0530

    change the name to simple social media app

--- src/main/java/com/shashikanth/OnlineSchool/SimpleSocialMediaApplication.java
+++ src/main/java/com/shashikanth/OnlineSchool/SimpleSocialMediaApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineSchoolApplication {
+public class SimpleSocialMediaApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		SpringApplication.run(OnlineSchoolApplication.class, args);
+		SpringApplication.run(SimpleSocialMediaApplication.class, args);
 	}
 
 }
--- src/test/java/com/shashikanth/OnlineSchool/SimpleSocialMediaApplicationTests.java
+++ src/test/java/com/shashikanth/OnlineSchool/SimpleSocialMediaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineSchoolApplicationTests {
+class SimpleSocialMediaApplicationTests {
 
 	@Test
 	void contextLoads() {
